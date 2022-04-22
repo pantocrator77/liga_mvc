2e198e1e06dc6d26bdefbc2aaf8452f8bb581660
pantocrator77 <lorenzo.vecchia@gmail.com>
2022-04-22 11:55:02 +0200

    last changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,9 @@
 <body>
     <h1>Esta es la homepage de {{ $ClubName }}</h1>
     
-    <p> <a href ="">{{ $Team1  }}</a></p>
+
+    @foreach ($data as $team)
+    <p> <a href ="teams">{{ $Team1  }}</a></p>
     <p> <a href ="">{{ $Team2  }}</a></p>
     <p> <a href ="">{{ $Team3  }}</a></p>
     <p> <a href ="">{{ $Team4  }}</a></p>
